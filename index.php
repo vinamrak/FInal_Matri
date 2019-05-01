59f7f109b37c61136c4f734c48d9f452ac6694d5
beast02 <iit2017029@iiita.ac.in>
2019-05-01 10:05:04 +0530

    First

---
+++ index.php
@@ -0,0 +1,187 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Find Your Perfect Partner - Make My Love | Home :: 
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script src="js/typed.js"></script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!--font-Awesome-->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!--font-Awesome-->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+ <?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="banner">
+  <div class="container" style="width:900px; margin-left:80px; margin-right:10px">
+    <div class="banner_info">
+      
+			<div id="typed-strings">
+    <p >Connecting Million Of Hearts</p>
+
+</div>
+<span id="typed" style="font-family:Georgia; font-size:50px;"></span>
+<script>
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+		smartBackspace: true ,
+		backSpeed: 5,
+		typeSpeed: 90,
+		loop:true
+  });
+</script>
+    </div>
+		<div style="margin-top:40px;">
+		<a href="register.php" class="hvr-shutter-out-horizontal btn11">Create your Profile</a>
+		</div>	
+  </div>
+  <div class="container" style="position:absolute; width:600px; height:500px; right:0; top:0; margin-top: 80px;">
+		
+		<img style="height:500px; width:500px; overflow-x:hidden;" src="/images/undraw.svg" >
+				
+  </div>	
+</div>
+
+<div class="grid_1">
+      <div class="container">
+      	<h1>Featured Profiles</h1>
+       	<div class="heart-divider">
+			<span class="grey-line"></span>
+			<i class="fa fa-heart pink-heart"></i>
+			<i class="fa fa-heart grey-heart"></i>
+			<span class="grey-line"></span>
+        </div>
+        <ul id="flexiselDemo3">
+        <?php
+        	$sql="SELECT * FROM customer";
+        	$result=mysqlexec($sql);
+        	if($result){
+        		while($row=mysqli_fetch_assoc($result)){
+        			$name=$row['firstname'] . " " . $row['lastname'];
+        			$profileid=$row['cust_id'];
+        			$age=$row['age'];
+        			$place=$row['state'] . "," . $row['district'];
+        			$job=$row['occupation'];
+
+        				//getting profilepic
+        				$pic1='';
+						$sql2="SELECT * FROM photos WHERE cust_id = $profileid";
+						$result2 = mysqlexec($sql2);
+						if($result2){
+							$row2=mysqli_fetch_array($result2);
+							$pic1=$row2['pic1'];
+						}
+						//got profilepic
+						//
+					//Printing the html
+					echo "<li><div class=\"col_1\"><a href=\"view_profile.php?id={$profileid}\">";
+					echo "<img src=\"profile/{$profileid}\/{$pic1}\" alt=\"\" class=\"hover-animation image-zoom-in img-responsive\"/>";
+					echo "<div class=\"layer m_1 hidden-link hover-animation delay1 fade-in\">";
+					echo "<div class=\"center-middle\">About {$name}</div>";
+					echo "</div>";
+					echo "<h3><span class=\"m_3\">Profile ID : {$profileid}</span><br>{$age}, {$place}<br>{$job}</h3></a></div>";
+					echo "</li>";
+				
+        		}
+        	}
+
+        ?>
+          </ul>
+	    <script type="text/javascript">
+		 $(window).load(function() {
+			$("#flexiselDemo3").flexisel({
+				visibleItems: 6,
+				animationSpeed: 1000,
+				autoPlay:false,
+				autoPlaySpeed: 3000,    		
+				pauseOnHover: true,
+				enableResponsiveBreakpoints: true,
+		    	responsiveBreakpoints: { 
+		    		portrait: { 
+		    			changePoint:480,
+		    			visibleItems: 1
+		    		}, 
+		    		landscape: { 
+		    			changePoint:640,
+		    			visibleItems: 2
+		    		},
+		    		tablet: { 
+		    			changePoint:768,
+		    			visibleItems: 3
+		    		}
+		    	}
+		    });
+		    
+		});
+	   </script>
+	   <script type="text/javascript" src="js/jquery.flexisel.js"></script>
+    </div>
+</div>
+
+	
+    <div class="footer">
+    	<div class="container">
+    		<div class="col-md-4 col_2">
+    			<h4><b>About Us</b></h4>
+    			<p>"Welcome to Marital.com Every life has its own charm in each stage and a married life is no exception. Marriage is an institution, which initiates the authentic values of life. In India, a marriage is usually arranged by the family or Relatives of the bride or the groom within a known circle. But today~s nomad life has narrowed this choice and hence matrimonial bureaus and marriage websites lead a vital role. Marital.com is celebrated as the most Trusted Matrimony Brand combining tradition and technology."</p>
+    		</div>
+    		<div class="col-md-2 col_2">
+    			
+    		</div>
+    		<div class="col-md-2 col_2">
+    			
+    		</div>
+    		<div class="col-md-2 col_2">
+    			<h4>Social</h4>
+    			<ul class="footer_social">
+				  <li><a href="#"><i class="fa fa-facebook fa1"> </i></a></li>
+				  <li><a href="#"><i class="fa fa-twitter fa1"> </i></a></li>
+				  <li><a href="#"><i class="fa fa-google-plus fa1"> </i></a></li>
+				  <li><a href="#"><i class="fa fa-youtube fa1"> </i></a></li>
+			    </ul>
+    		</div>
+    		<div class="clearfix"> </div>
+    		<div class="copy">
+			  <p>Designed by 
+			  <div class="heart-divider">
+				<span class="grey-line"></span>
+				<i class="fa fa-heart pink-heart"></i>
+				<i class="fa fa-heart grey-heart"></i>
+				<span class="grey-line"></span>
+			</div>
+			</p>
+	        </div>
+    	</div>
+    </div>
+</body>
+</html>	
